da76e64c1471caf52f9eb939785b2aa298cb2b57
quinnzaballa <quinnmatthewczaballa@gmail.com>
2024-11-13 22:22:35 +0800

    Serial Communication Between ESP32 and Arduino.
    Undefined

---
+++ Serial_Communication/SerialComms_1-Arduino/SerialComms_1-Arduino.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+// Define the pin numbers for UART communication
+#define RX_PIN 10
+#define TX_PIN 11
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX pins
+
+// Unique ID and Auth Token
+String deviceID = "Arduino_001";
+String authToken = "AUTH1234";
+
+// Data to send
+String message = "Hello from Arduino!";
+
+void setup() {
+  Serial.begin(9600);   // Serial monitor
+  mySerial.begin(9600); // UART communication with ESP32
+  
+  Serial.println("Initializing Arduino...");
+
+  // Send ID and Authentication token
+  sendMessage();
+}
+
+void loop() {
+  // Send data periodically
+  delay(5000);  // Send every 5 seconds
+  sendMessage();
+}
+
+void sendMessage() {
+  String data = "ID:" + deviceID + ", AUTH:" + authToken + ", MSG:" + message;
+  mySerial.println(data); // Send the data to ESP32
+  Serial.println("Sent: " + data); // Print to Serial Monitor
+}
